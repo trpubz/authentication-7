f0c2d1c6568794ff5e9fdb5c4758a4a13128e1e0
Michael Dao <mike@turing.edu>
2023-02-14 09:57:07 -0700

    adds current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     new_user = User.create(user_params)
+    session[:user_id] = new_user.id
     flash[:success] = "Welcome, #{new_user.username}!"
     redirect_to root_path
   end
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(username: params[:username])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
       redirect_to root_path
     else
