b46de45193256af145ab3ba0b0a7bfc55c0ebdbc
Taylor Pubins <tpubz@icloud.com>
2023-12-12 10:57:50 -0700

    Add sad path for default user trying to access admin dashboard

--- spec/features/admin/login_spec.rb
+++ spec/features/admin/login_spec.rb
@@ -16,3 +16,17 @@ RSpec.describe 'admin login' do
     end
   end
 end
+
+describe "as default user" do
+  it 'does not allow default user to see admin dashboard index' do
+    user = User.create(username: "fern@gully.com",
+                       password: "password",
+                       role: 0)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
+end
